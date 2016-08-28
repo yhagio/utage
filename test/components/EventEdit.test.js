867ab9ebe7b721656cceb35ec514dc2c1674f01d
Yuichi Hagio <yhagio87@gmail.com>
2016-08-28 00:42:32 -0400

    EventEdit test setup

--- test/components/EventEdit.test.js
+++ test/components/EventEdit.test.js
@@ -1,8 +1,29 @@
-// import React from 'react';
-// import { expect } from 'chai';
-// import { shallow } from 'enzyme';
-// import EventEdit from '../../src/components/EventEdit/EventEdit';
+import React from 'react';
+import { Map } from 'immutable';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import EventEdit from '../../src/components/EventEdit/EventEdit';
 
-// describe('Component: EventEdit', () => {
-  
-// });
+describe('Component: EventEdit', () => {
+  it('displays the correct page');
+  it('displays loading message when it is loading');
+  it('update the title');
+  it('warns if the title is empty');
+  it('update the description');
+  it('warns if the description is empty');
+  it('update the price');
+  it('update the address');
+  it('warns if the address is empty');
+  it('update the startDate');
+  it('warns if the startDate is empty');
+  it('update the endDate');
+  it('warns if the endDate is empty');
+  it('update the category');
+  it('can update if all fields are filled');
+  it('can delete on clicking delete button');
+});
+
+describe('Component: SubmitButton', () => {
+  it('if errors, cannot update');
+  it('if no errors, can update');
+});
